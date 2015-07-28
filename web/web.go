dc829e6a1ddce6c4d0e2e69211c8461bcc925e21
David Thien <therealdavidthien@gmail.com>
2015-07-28 13:01:25 -0700

    Started reworking handler function for non-mux based handling.

--- web/web.go
+++ web/web.go
@@ -14,27 +14,41 @@ type myHandler struct {
 }
 
 func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := mux[r.URL.String()]; ok {
-		h(w, r)
-		return
-	}
-	io.WriteString(w, "My server: "+r.URL.String())
+	path := r.URL.Path[1:]
+  log.Println(path)
+
+  data, err := ioutil.ReadFile(string(path))
+
+  if err == nil {
+    w.Write(data)
+  } else {
+    w.WriteHeader(404)
+    w.Write([]byte("404 error find another page. " + http.StatusText(404)))
+  }
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  page, _ := ioutil.ReadFile("test.html")
+  page, _ := ioutil.ReadFile("templates/test.html")
   io.WriteString(w, string(page))
 }
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
 //TODO: Error handling
+
+//old
+/*
 func returnMuxFunc(fileName string) func(http.ResponseWriter, *http.Request) {
   return func(w http.ResponseWriter, r *http.Request) {
     page, _ :=ioutil.ReadFile(fileName)
     io.WriteString(w, string(page))
   }
 }
-//TODO: Error handling
+*/
+
+
+//old code for using a mux
+/*
+//todo: Error handling
 func createMux(fileName string) map[string]func(http.ResponseWriter, *http.Request) {
   m := make(map[string]func(http.ResponseWriter, *http.Request))
 
@@ -45,11 +59,12 @@ func createMux(fileName string) map[string]func(http.ResponseWriter, *http.Reque
     curLine := strings.Split(scanner.Text(), ":")
     m[curLine[0]] = returnMuxFunc(curLine[1])
   }
-  //TODO: Switch to looking for .css files rather than in templates
+  //todo: Switch to looking for .css files rather than in templates
   tempHandler := http.StripPrefix("/templates/", http.FileServer(http.Dir("/templates")))
   m["templates"] = tempHandler.ServeHTTP
   return m
 }
+*/
 
 //TODO: Implement fileserver for css ↓
 //https://groups.google.com/forum/#!topic/golang-nuts/aGMLK_2OHiM
