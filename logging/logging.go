ae05723caa495f55dfb364e1f55b70e18d4c6cba
David Thien <therealdavidthien@gmail.com>
2015-08-10 14:48:07 -0700

    More work on logging

--- logging/logging.go
+++ logging/logging.go
@@ -15,14 +15,20 @@ func check(e error) {
 
 func LogWebPath(requestedURL string, accessedURL string, logFile string) error {
   //TODO: Ensure file and directory exist
-
-  file, err := os.Create(logFile)
+  //TODO: Separate file for error logging
+  //NOTE: First parameter is FileInfo
+  _, err := os.Stat(logFile)
+  if(err != nil) {
+    //TODO: Separate file for error logging
+    //NOTE: os.Create returns type File
+    fcreated, err := os.Create(logFile)
+    fcreated.Close()
+    fmt.Println(err)
+  }
 
   //TODO: Separate file for error logging
-  if(err != nil) {
-    fmt.Println(err)
-    return err
-  }
+  //NOTE: The file should exist here
+  file, _ := os.Open(logFile)
 
   //TODO: Include timestamp
   //TODO: Include ip?
