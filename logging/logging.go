0dbe7897a4877db302cc62be0b814049d3f2d2ee
David Thien <therealdavidthien@gmail.com>
2015-08-12 15:57:10 -0700

    Fixed logging to write and append to files now

--- logging/logging.go
+++ logging/logging.go
@@ -5,13 +5,18 @@ import (
   "fmt"
   "os"
   "io"
+  //"syscall"
 )
+
+//TODO: Decide if should use a logger struct
 
 func check(e error) {
   if(e != nil) {
     panic(e)
   }
 }
+
+//TODO: Make functions for checking and opening files
 
 func LogWebPath(requestedURL string, accessedURL string, logFile string) error {
   //TODO: Ensure file and directory exist
@@ -28,12 +33,14 @@ func LogWebPath(requestedURL string, accessedURL string, logFile string) error {
 
   //TODO: Separate file for error logging
   //NOTE: The file should exist here
-  file, _ := os.Open(logFile)
+  file, _ := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY, 0666)
 
   //TODO: Include timestamp
   //TODO: Include ip?
   //NOTE: First return value returns number of bytes writen
-  _, err = io.WriteString(file, requestedURL + " was requested | " + accessedURL + " was accessed")
+  //TODO: Additional info for 404
+  //TODO: Rework to give better info
+  _, err = io.WriteString(file, requestedURL + " was requested | " + accessedURL + " was accessed\n")
   //TODO: Separate file for error logging
   if(err != nil) {
     fmt.Println(err)
@@ -41,4 +48,26 @@ func LogWebPath(requestedURL string, accessedURL string, logFile string) error {
   }
   file.Close()
   return errors.New("nil")
+}
+
+func logErrors(toWrite string, logFile string) error {
+  _, err := os.Stat(logFile)
+  if(err != nil) {
+    fcreated, err :=os.Create(logFile)
+    fcreated.Close()
+    fmt.Println(err)
+  }
+  //TODO: Check syscall.O_RDWR
+  file, _ := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY, 0666)
+
+  //TODO: Include timestamp
+  //NOTE: First return value returns number of bytes writen
+  _, err = io.WriteString(file, toWrite + "\n")
+
+  if(err != nil) {
+    fmt.Println("Error writing to log file!")
+    fmt.Println(err)
+    return err
+  }
+  return err
 }
